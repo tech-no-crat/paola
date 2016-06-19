c9d4a26b9887063e0c22567d13d56e31c9c5951d
Christos Porios <christos.porios@gmail.com>
2016-06-19 21:56:14 +0300

    Make testing script detect assembler errors.

--- run_tests.sh
+++ run_tests.sh
@@ -11,7 +11,7 @@ run_test () {
   expect=`cat $test | sed -En 's/.*@EXPECT ([0-9]+)/\1/p'`
   total=$((total+1))
 
-  ($comp $test > /dev/null)
+  ($comp $test)
   compstatus=$?
   if [ $compstatus -ne 0 ]; then
     echo "$REDCOL FAIL$NOCOL $test:\n\tCompilation returned non-zero exit status $compstatus."
@@ -20,6 +20,13 @@ run_test () {
   fi
 
   g++ out.s -o out
+  assemblestatus=$?
+  if [ $assemblestatus -ne 0 ]; then
+    echo "$REDCOL FAIL$NOCOL $test:\n\tAssembling returned non-zero exit status $assemblestatus."
+    fail=$((fail+1))
+    return;
+  fi
+
   (./out)
   actual=$?
   if [ $actual -ne $expect ]; then
